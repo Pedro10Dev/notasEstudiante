d9891747cbdda2a117a0e1d5a1e765c7409f7cfa
Pedro Jose <pedroarellano9810@gmail.com>
2024-08-14 01:34:54 -0400

    Primer commit

---
+++ src/test/java/com/example/notasestudiantes/NotasestudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notasestudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasestudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
